2b52a48438bb90800bb2b170867f5483b94948b6
Ashwani Kumar <84488161+TheMannu@users.noreply.github.com>
2025-01-23 16:45:35 +0530

    Update AWS-CLI-Setup.sh

--- AWSCLI/AWS-CLI-Setup.sh
+++ AWSCLI/AWS-CLI-Setup.sh
@@ -39,9 +39,3 @@ rm -f awscliv2.zip
 rm -rf aws/
 
 echo "AWS CLI setup script completed successfully!"
-
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo apt install unzip
-unzip awscliv2.zip
-sudo ./aws/install
-aws configure
