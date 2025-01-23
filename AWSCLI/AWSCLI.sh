a27fd404dbb594f2129b95a7622d52087e0b353b
Ashwani Kumar <84488161+TheMannu@users.noreply.github.com>
2025-01-23 16:39:56 +0530

    Update AWSCLI.sh

--- AWSCLI/AWSCLI.sh
+++ AWSCLI/AWSCLI.sh
@@ -7,6 +7,11 @@ echo "Starting AWS CLI setup script..."
 # Update the package index
 echo "Updating package index..."
 sudo apt-get update -y
+
+# Ensure necessary tools are installed
+echo "Installing required packages..."
+sudo apt-get install -y unzip curl
+
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt install unzip
 unzip awscliv2.zip
