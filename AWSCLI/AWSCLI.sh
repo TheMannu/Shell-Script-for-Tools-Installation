4ce6ab454b455b78f615aafff3b88a67c353da9f
Ashwani Kumar <84488161+TheMannu@users.noreply.github.com>
2025-01-23 16:41:33 +0530

    Update AWSCLI.sh

--- AWSCLI/AWSCLI.sh
+++ AWSCLI/AWSCLI.sh
@@ -12,6 +12,16 @@ sudo apt-get update -y
 echo "Installing required packages..."
 sudo apt-get install -y unzip curl
 
+# Remove older AWS CLI installations
+echo "Removing old AWS CLI installations if they exist..."
+sudo apt-get remove -y awscli || true
+sudo snap remove aws-cli || true
+
+# Download AWS CLI v2 installation package
+echo "Downloading AWS CLI v2..."
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt install unzip
 unzip awscliv2.zip
